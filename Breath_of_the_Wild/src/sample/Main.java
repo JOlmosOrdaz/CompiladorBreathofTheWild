660791fee1c4c440194cedc5d32dfc9e64e6a717
JOlmosOrdaz <31020969+JOlmosOrdaz@users.noreply.github.com>
2018-10-03 19:44:37 -0600

    modificacion de la medida
    dasdafa

--- Breath_of_the_Wild/src/sample/Main.java
+++ Breath_of_the_Wild/src/sample/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("principal.fxml"));
         primaryStage.setTitle("Breath_of_the_wild");
-        primaryStage.setScene(new Scene(root, 960, 600));
+        primaryStage.setScene(new Scene(root, 970, 600));
         primaryStage.show();
     }
 
