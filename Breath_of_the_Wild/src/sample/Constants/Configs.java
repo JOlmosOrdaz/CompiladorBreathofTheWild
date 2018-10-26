48edb18a2faef15fbb764cc0f024ffabd5c74bed
JOlmosOrdaz <31020969+JOlmosOrdaz@users.noreply.github.com>
2018-10-25 19:51:41 -0600

    Revicion de Expreciones
    Revicionde Expreciones

---
+++ Breath_of_the_Wild/src/sample/Constants/Configs.java
@@ -0,0 +1,68 @@
+package sample.Constants;
+
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class Configs {
+    public static StyleSpans<Collection<String>> computeHighlighting(String text) {
+        Matcher matcher = PATTERN.matcher(text);
+        int lastKwEnd = 0;
+        StyleSpansBuilder<Collection<String>> spansBuilder
+                = new StyleSpansBuilder<>();
+        while(matcher.find()) {
+            String styleClass =
+                    matcher.group("KEYWORD") != null ? "keyword" :
+                            matcher.group("PAREN") != null ? "paren" :
+                                    matcher.group("BRACE") != null ? "brace" :
+                                            matcher.group("BRACKET") != null ? "bracket" :
+                                                    matcher.group("SEMICOLON") != null ? "semicolon" :
+                                                            matcher.group("STRING") != null ? "string" :
+                                                                    matcher.group("COMMENT") != null ? "comment" :
+                                                                            null; /* never happens */ assert styleClass != null;
+            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
+            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
+            lastKwEnd = matcher.end();
+        }
+        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
+        return spansBuilder.create();
+    }
+    public static String[] EXPRESIONES={
+            "Tengo ([Runes]|[])+ con poder [0-9]{2} ",
+            "Mis [A-Za-z]{1,3}+ Contienen [A-Za-z]{1,3}+ Con Capacidad [0-9]{2}",
+            "/**/comentario",
+            "//comentario 2"
+
+    };
+    public static final String[] KEYWORDS = new String[] {
+            "Runes","RemoteBomb","Magnesis","Stasis","Cryonis","Camera","MClicleZ",
+            "Amiibo","ModulosSheika","Bombaremota","Magnetismo","Paralisis","Tempano",
+            "Camara","BowandArrows","MasterSword"
+    };
+
+    public static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
+    public static final String PAREN_PATTERN = "\\(|\\)";
+    public static final String BRACE_PATTERN = "\\{|\\}";
+    public static final String BRACKET_PATTERN = "\\[|\\]";
+    public static final String SEMICOLON_PATTERN = "\\;";
+    public static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
+    public static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/";
+
+    public static final Pattern PATTERN = Pattern.compile(
+            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
+                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
+                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
+                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
+                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
+                    + "|(?<STRING>" + STRING_PATTERN + ")"
+                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
+    );
+
+    public static final String sampleCode = String.join("\n", new String[] {
+
+    });
+}
--- Breath_of_the_Wild/src/sample/Controller.java
+++ Breath_of_the_Wild/src/sample/Controller.java
@@ -4,7 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.fxmisc.richtext.CodeArea;
@@ -20,70 +23,28 @@ import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import sample.Constants.Configs.*;
+
+import static sample.Constants.Configs.EXPRESIONES;
+import static sample.Constants.Configs.computeHighlighting;
+import static sample.Constants.Configs.sampleCode;
+
 public class Controller{
     private Stage stage;
 
-    @FXML private Pane paneSote;
+    @FXML
+    TextArea txtConsola;
 
-    private static final String[] KEYWORDS = new String[] {
-            "abstract", "assert", "boolean", "break", "byte",
-            "case", "catch", "char", "class", "const",
-            "continue", "default", "do", "double", "else",
-            "enum", "extends", "final", "finally", "float",
-            "for", "goto", "if", "implements", "import",
-            "instanceof", "int", "interface", "long", "native",
-            "new", "package", "private", "protected", "public",
-            "return", "short", "static", "strictfp", "super",
-            "switch", "synchronized", "this", "throw", "throws",
-            "transient", "try", "void", "volatile", "while"
-    };
+    @FXML private HBox paneSote;
 
-    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
-    private static final String PAREN_PATTERN = "\\(|\\)";
-    private static final String BRACE_PATTERN = "\\{|\\}";
-    private static final String BRACKET_PATTERN = "\\[|\\]";
-    private static final String SEMICOLON_PATTERN = "\\;";
-    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
-    private static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/";
 
-    private static final Pattern PATTERN = Pattern.compile(
-            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
-                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
-                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
-                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
-                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
-                    + "|(?<STRING>" + STRING_PATTERN + ")"
-                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
-    );
-
-    private static final String sampleCode = String.join("\n", new String[] {
-            "package com.example;",
-            "",
-            "import java.util.*;",
-            "",
-            "public class Foo extends Bar implements Baz {",
-            "",
-            "    /*",
-            "     * multi-line comment",
-            "     */",
-            "    public static void main(String[] args) {",
-            "        // single-line comment",
-            "        for(String arg: args) {",
-            "            if(arg.length() != 0)",
-            "                System.out.println(arg);",
-            "            else",
-            "                System.err.println(\"Warning: empty string as argument\");",
-            "        }",
-            "    }",
-            "",
-            "}"
-    });
 
     CodeArea codeArea = new CodeArea();
     @FXML protected void initialize(){
         // add line numbers to the left of area
         codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
         codeArea.replaceText(0, 0, sampleCode);
+
         codeArea.setPrefSize(800,500);
 
         // recompute the syntax highlighting 500 ms after user stops editing area
@@ -92,6 +53,7 @@ public class Controller{
                 .successionEnds(Duration.ofMillis(500))
                 .subscribe(ignore -> codeArea.setStyleSpans(0, computeHighlighting(codeArea.getText())));
 
+        HBox.setHgrow(codeArea, Priority.ALWAYS);
         paneSote.getChildren().add(codeArea);
 
     }
@@ -101,8 +63,8 @@ public class Controller{
     }
     public void evtAbrir(ActionEvent event){
         FileChooser of=new FileChooser();
-        of.setTitle("Abrir archivo Food Compiler yea");
-        FileChooser.ExtensionFilter filtro= new FileChooser.ExtensionFilter("Archivos .food","*.food");
+        of.setTitle("Abrir archivo BOTW");
+        FileChooser.ExtensionFilter filtro= new FileChooser.ExtensionFilter("Archivos .BOTW","*.BOTW");
         of.getExtensionFilters().add(filtro);
         File file=of.showOpenDialog(stage);
     }//llave abrir
@@ -110,26 +72,40 @@ public class Controller{
     public void start(Stage stage) throws Exception {
         this.stage=stage;
     }
-    private static StyleSpans<Collection<String>> computeHighlighting(String text) {
-        Matcher matcher = PATTERN.matcher(text);
-        int lastKwEnd = 0;
-        StyleSpansBuilder<Collection<String>> spansBuilder
-                = new StyleSpansBuilder<>();
-        while(matcher.find()) {
-            String styleClass =
-                    matcher.group("KEYWORD") != null ? "keyword" :
-                            matcher.group("PAREN") != null ? "paren" :
-                                    matcher.group("BRACE") != null ? "brace" :
-                                            matcher.group("BRACKET") != null ? "bracket" :
-                                                    matcher.group("SEMICOLON") != null ? "semicolon" :
-                                                            matcher.group("STRING") != null ? "string" :
-                                                                    matcher.group("COMMENT") != null ? "comment" :
-                                                                            null; /* never happens */ assert styleClass != null;
-            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
-            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
-            lastKwEnd = matcher.end();
-        }
-        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
-        return spansBuilder.create();
+
+    public void ejecutar(ActionEvent event){
+        compilar();
+    }///LLAVE EJECUTAR
+    public void compilar(){
+        txtConsola.setText("");
+        long tInicial=System.currentTimeMillis();
+
+        String texto=codeArea.getText();
+        String[] renglones=texto.split("\\n");
+
+        for(int x=0;x<renglones.length;x++){
+            boolean bandera=false;
+            if (!renglones[x].trim().equals("")){
+                for(int y=0;y< EXPRESIONES.length && bandera==false;y++){
+                    Pattern patron=Pattern.compile(EXPRESIONES[y]);
+                    Matcher matcher=patron.matcher(renglones[x]);
+                    if(!matcher.matches()){
+                        bandera=true;
+
+                    }
+                 }
+            }
+
+            if(bandera==false){
+                txtConsola.setText(
+                        txtConsola.getText()+" \n"+
+                                "Error de sintaxis en la linea "+(x+1));
+            }//llave if
+        }//llave for
+        long tFinal=System.currentTimeMillis()-tInicial;
+        txtConsola.setText(txtConsola.getText()+"\n"+
+                "Compilador en :"+tFinal+" Milisegundos");
+
     }
+
 }
